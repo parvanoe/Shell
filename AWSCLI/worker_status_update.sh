ecfb0a34398b06513f40bbc152e65f34fdf20ed1
Emil Parvanov <100536596+parvanoe@users.noreply.github.com>
2025-01-09 15:16:12 +0200

    Update worker_status_update.sh

--- AWSCLI/worker_status_update.sh
+++ AWSCLI/worker_status_update.sh
@@ -7,7 +7,7 @@ status=$(aws eks describe-nodegroup --cluster-name <clustername> --region <regio
 while [[ "$status" == "UPDATING" ]]; do
     echo "Nodegroup is still updating..."
     sleep 5
-    status=$(aws eks describe-nodegroup --cluster-name EKS-SOF-01-test --region eu-central-1 --nodegroup-name EKS-SOF-01-Nodegroup --query "nodegroup.status" --output text)
+    status=$(aws eks describe-nodegroup --cluster-name <clustername> --region <region> --nodegroup-name <nodegroup> --query "nodegroup.status" --output text)
 done
 
 echo "Nodegroup update completed with status: $status"
